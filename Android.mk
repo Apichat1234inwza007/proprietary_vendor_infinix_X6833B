53a20202f203c0ebd6e86a78460694a34052521b
Apichat1234inwza007 <discom610@gmail.com>
2026-04-01 22:13:00 +0700

    X6833B: Initial skeleton
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),X6833B)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,070d542ebcf8585c2aeeb61aae8ed62512f781f3)
+$(call add-radio-file-sha1-checked,radio/gz.img,a1a067fd6e43151be5bf8ba9e6380d94a01534c3)
+$(call add-radio-file-sha1-checked,radio/lk.img,63232ca296155867aee64542d5a81baad399d743)
+$(call add-radio-file-sha1-checked,radio/logo.img,6880e4d79553d71080b80a0224d6a0e1ea21aa91)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,602e60cd2f698d880ccb2556ef50d824dbef506d)
+$(call add-radio-file-sha1-checked,radio/md1img.img,ef8ad7e8805e4df534c8030c35124a0158909bea)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,8760971ea7b035a4d72203646977e2983b46d439)
+$(call add-radio-file-sha1-checked,radio/scp.img,617c60c531f8dbc081839f61f607920634c4b954)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,74fe938a136837875b61df7001a5b4bb78a5c099)
+$(call add-radio-file-sha1-checked,radio/sspm.img,3cc6bd95266de109e8d286571ede511ee887fea0)
+$(call add-radio-file-sha1-checked,radio/tee.img,8933a18a8cf76b8b6929a0574224a2321ca2aaa3)
+$(call add-radio-file-sha1-checked,radio/tkv.img,3cefad774a28d72f2dc0947e946dfd3c2359e9ba)
+
+endif
